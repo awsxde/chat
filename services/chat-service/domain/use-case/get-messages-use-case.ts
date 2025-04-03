a2036feaaa828c7c1663400cad7691bc47ee2526
ali <ali@gmail.com>
2025-04-03 15:42:05 +0330

    feat: add get messages from chat room tests

--- services/chat-service/domain/use-case/get-messages-use-case.ts
+++ services/chat-service/domain/use-case/get-messages-use-case.ts
@@ -2,9 +2,7 @@ import * as chatRepository from '../../data-access/chat-repository';
 import { throwIfChatRoomNotExists } from '../validation/validate-room-existence';
 
 export async function getMessages(chatRoomId: number) {
-  throwIfChatRoomNotExists(chatRoomId);
-
+  await throwIfChatRoomNotExists(chatRoomId);
   const response = await chatRepository.getMessages(chatRoomId);
-
   return response;
 }
