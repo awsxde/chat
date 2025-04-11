a8a6c27bf8d68976588c07edd26fba9a90887861
ali <ali@gmail.com>
2025-04-11 13:59:09 +0330

    fix: remove extra validation on get chat room

--- services/chat-service/domain/use-case/get-chat-room-use-case.ts
+++ services/chat-service/domain/use-case/get-chat-room-use-case.ts
@@ -1,10 +1,17 @@
+import { AppError } from '@practica/error-handling';
 import * as chatRepository from '../../data-access/chat-repository';
-import { throwIfChatRoomNotExists } from '../validation/validate-room-existence';
 
 export async function getChatRoom(chatRoomId: number) {
-  await throwIfChatRoomNotExists(chatRoomId);
-
   const response = await chatRepository.getChatRoomById(chatRoomId);
+
+  if (!response) {
+    throw new AppError(
+      'chat-room-does-not-exist',
+      `The chat room with id ${chatRoomId} does not exist`,
+      404,
+      false
+    );
+  }
 
   return response;
 }
