5d904b0902b5bcbbab924ec23513ee8754a9e8b4
ali <ali@gmail.com>
2025-04-12 10:11:57 +0330

    fix: update function names base on naming convection in auth service

--- services/auth-service/test/login-user.test.ts
+++ services/auth-service/test/login-user.test.ts
@@ -3,19 +3,15 @@ import sinon from 'sinon';
 import { startWebServer, stopWebServer } from '../entry-points/api/server';
 import * as testHelpers from './test-helpers';
 
-// Configuring file-level HTTP client with base URL will allow
-// all the tests to approach with a shortened syntax
 let axiosAPIClient;
 
 beforeAll(async () => {
   process.env.JWT_TOKEN_SECRET = testHelpers.exampleSecret;
-  // ️️️✅ Best Practice: Place the backend under test within the same process
   const apiConnection = await startWebServer();
   const axiosConfig = {
     baseURL: `http://127.0.0.1:${apiConnection.port}`,
-    validateStatus: () => true, // Don't throw HTTP exceptions. Delegate to the tests to decide which error is acceptable
+    validateStatus: () => true,
     headers: {
-      // ️️️✅ Best Practice: Test like production, include real token to stretch the real authentication mechanism
       authorization: testHelpers.signValidTokenWithDefaultUser(),
     },
   };
@@ -23,7 +19,6 @@ beforeAll(async () => {
 });
 
 beforeEach(() => {
-  // ️️️✅ Best Practice: Start each test with a clean slate
   sinon.restore();
 });
 
@@ -85,5 +80,3 @@ describe('POST /auth/login', () => {
     expect(response.status).toBe(400);
   });
 });
-
-export {};
